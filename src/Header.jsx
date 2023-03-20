3946eb0a8bc8105c355f20f034f59ed5f3163789
Ailin Garcia <agarcia@switch.com.uy>
2023-03-20 12:25:47 -0300

    header changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,14 @@
-import styles from './Header.module.css'
+import './Header.css'
+
+// Header is a stateless component that displays the header title and the Rick and Morty logo image with a link to a new tab
+// with the Rick and Morty API documentation
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <p className={styles.text}>Rick and Morty characters</p>
+    <div className='header-container'>
+      <p className='header-text'>Rick and Morty characters</p>
       <a href='https://rickandmortyapi.com/documentation/' target='_blank'>
-        <img className={styles.logo} src='./log1.png' alt='logo' />
+        <img className='header-logo' src='./logo-header.png' alt='logo' />
       </a>
     </div>
   )
