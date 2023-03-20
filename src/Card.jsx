264a1485a066086e5fb8f28bc6d8c61607ab557c
Ailin Garcia <agarcia@switch.com.uy>
2023-03-20 18:46:29 -0300

    Card component changes

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,29 @@
-import styles from './Card.module.css'
+import './Card.css'
 
+/**
+ * Card component
+ * Stateless component to show a card with the character info
+ * @prop {object} details - Object with the character data (name, status, species, gender, location, type)
+ * @prop {function} onCardClose - Function to be executed in the parent (Table) to close the card
+ */
 function Card({ details, onCardClose }) {
-  const labels = ['Name', 'Species', 'Gender', 'Status', 'Type']
-
-  const cardContent = labels.map((label) => {
-    return (
-      <div key={label}>
-        <label>{label} </label>
-        <span>{details[label.toLowerCase()] || '-'}</span>
-      </div>
-    )
-  })
+  const labels = ['Name', 'Species', 'Gender', 'Status', 'Location', 'Type']
 
   const card = (
-    <aside key={1} className={styles.container}>
-      <img className={styles.image} src={details.image} alt='character image' />
-      <div className={styles.info}>{cardContent}</div>
-      <button className={styles.closeBtn} onClick={onCardClose}>
+    <aside className='card-container'>
+      <img className='card-image' src={details.image} alt='character image' />
+      <section className='card-info'>
+        {labels.map((label) => {
+          const detailData = label === 'Location' ? details[label.toLowerCase()].name : details[label.toLowerCase()]
+          return (
+            <div className={label === 'Name' ? 'card-content-group name' : 'card-content-group'} key={label}>
+              {label !== 'Name' && <label className='card-label'>{label} </label>}
+              <span>{detailData || '-'}</span>
+            </div>
+          )
+        })}
+      </section>
+      <button className='card-close-btn' onClick={onCardClose}>
         Close
       </button>
     </aside>
